defe04ca4284f7fe89d731b6b7a2b68a57f99fc3
Muhammad Renaldy Ramadhan <mrenaldyr03@gmail.com>
2024-01-20 20:09:54 +0700

    feat: remove token max in route.ts and add clear button in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,23 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Markdown from 'react-markdown'
-import { Input } from "@/components/ui/input"
-import { MessageCircleCode, Upload,  } from "lucide-react";
-import { Send, Copy, Download } from "lucide-react";
+import Markdown from "react-markdown";
+import { Input } from "@/components/ui/input";
+import { MessageCircleCode, Upload } from "lucide-react";
+import { Send, Copy, Download, XCircle } from "lucide-react";
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 import toast, { Toaster } from "react-hot-toast";
-import styles from '../styles/styles.module.css'
+import styles from "../styles/styles.module.css";
 
 export default function Home() {
-  
   // state for the prompt, response and output
   const [prompt, setPrompt] = useState("");
   const [response, setResponse] = useState("");
   const [output, setOutput] = useState("The response will appear here...");
 
-  const onKeyDown = (e:any) => {
+  const onKeyDown = (e: any) => {
     // Check if the Ctrl key is pressed along with the Enter key
     if (e.key === "Enter") {
       // Prevent the default behavior of the Enter key (e.g., new line in textarea)
@@ -28,7 +27,7 @@ export default function Home() {
     }
   };
 
-  const onFileChange = (e:any) => {
+  const onFileChange = (e: any) => {
     // Get the file
     const file = e.target.files[0];
     // Check if the file is null
@@ -36,27 +35,22 @@ export default function Home() {
       toast.error("No file selected!");
       return;
     }
-    // Check if the file type is supported
-    if (!file.type.includes("text")) {
-      toast.error("File type not supported!");
-      return;
-    }
     // Read the file
     const reader = new FileReader();
-    reader.readAsText(file, "UTF-8");
+    reader.readAsDataURL(file);
     // On reader load
     reader.onload = (readerEvent) => {
       // Set the prompt to the file content
       // @ts-ignore
-      setPrompt(readerEvent.target?.result || "done");
+      setPrompt(readerEvent.result || "done");
     };
-  }
+  };
 
   const copyToClipboard = () => {
     // Copy the output to the clipboard
     navigator.clipboard.writeText(output);
     toast.success("Copied to clipboard!");
-  }
+  };
 
   const downloadFile = () => {
     // Create a new blob
@@ -74,17 +68,16 @@ export default function Home() {
     anchor.remove();
     // Revoke the URL
     window.URL.revokeObjectURL(url);
-  }
+  };
 
   const onSubmit = async () => {
-
+    // clear the output
+    setOutput("Generating response...");
     if (prompt === "") {
       toast.error("Prompt cannot be empty!");
+      setOutput("The response will appear here...");
       return;
     }
-
-    // clear the output
-    setOutput("The response will appear here...");
 
     // create a post request to the /api/chat endpoint
     const response = await fetch("api/chat", {
@@ -101,7 +94,6 @@ export default function Home() {
     const data = await response.json();
     // set the response in the state
     setResponse(data.text);
-
   };
 
   useEffect(() => {
@@ -109,13 +101,12 @@ export default function Home() {
     if (response.length === 0) return;
 
     setOutput("");
-    
+
     for (let i = 0; i < response.length; i++) {
       setTimeout(() => {
         setOutput((prev) => prev + response[i]);
       }, i * 10);
     }
-    
   }, [response]);
 
   return (
@@ -131,40 +122,75 @@ export default function Home() {
             type="text"
             placeholder="prompt"
             value={prompt}
-            className={cn("min-w-[320px] sm:min-w-[400px] md:min-w-[500px] h-[50px] pr-12")}
+            className={cn(
+              "min-w-[320px] sm:min-w-[400px] md:min-w-[500px] h-[50px] pr-12"
+            )}
             onChange={(e) => {
               setPrompt(e.target.value);
             }}
             onKeyDown={(e) => onKeyDown(e)}
           />
-          <button onClick={() => onSubmit()} className="absolute top-3 right-3 hover:scale-110 transition ease-in-out">
+          <button
+            onClick={() => onSubmit()}
+            className="absolute top-3 right-3 hover:scale-110 transition ease-in-out"
+          >
             <Send />
           </button>
         </div>
-        <Input type="file" onChange={(e) => onFileChange(e)} className="hidden" />
-        <Button variant="outline" className={cn("w-[40px] p-1")} onClick={() => {
-          const fileInput = document.querySelector("input[type=file]") as HTMLInputElement;
-          fileInput.click();
-        }}>
-          <Upload className={cn("w-[20px]")}/>
+        <Input
+          type="file"
+          onChange={(e) => onFileChange(e)}
+          className="hidden"
+        />
+        <Button
+          variant="outline"
+          className={cn("w-[40px] p-1")}
+          onClick={() => {
+            const fileInput = document.querySelector(
+              "input[type=file]"
+            ) as HTMLInputElement;
+            fileInput.click();
+          }}
+        >
+          <Upload className={cn("w-[20px]")} />
         </Button>
       </div>
       <div className="flex gap-3 items-center">
-        <Card className={cn("p-5 whitespace-normal min-w-[320px] sm:w-[500px] md:min-w-[600px] min-h-[150px] max-h-[400px] lg:min-w-[700px] overflow-y-scroll")}>
+        <Card
+          className={cn(
+            "p-5 whitespace-normal min-w-[320px] sm:w-[500px] md:min-w-[600px] min-h-[150px] max-h-[400px] lg:min-w-[700px] overflow-y-scroll"
+          )}
+        >
           <div className={`${styles.textwrapper}`}>
             <Markdown className={cn("w-full h-full ")}>{`${output}`}</Markdown>
           </div>
         </Card>
         <div className="flex flex-col gap-5">
-          <Button variant="outline" className={cn("w-[40px] p-1")} onClick={()=>copyToClipboard()}>
+          <Button
+            variant="outline"
+            className={cn("w-[40px] p-1")}
+            onClick={() => copyToClipboard()}
+          >
             <Copy className={cn("w-[20px]")} />
           </Button>
-          <Button variant="outline" className={cn("w-[40px] p-1")} onClick={()=>downloadFile()}>
+          <Button
+            variant="outline"
+            className={cn("w-[40px] p-1")}
+            onClick={() => downloadFile()}
+          >
             <Download className={cn("w-[20px]")} />
+          </Button>
+          <Button
+            variant="outline"
+            className={cn("w-[40px] p-1")}
+            onClick={() => {
+              setOutput("The response will appear here...");
+            }}
+          >
+            <XCircle className={cn("w-[20px]", "text-red-500")} />
           </Button>
         </div>
       </div>
     </main>
   );
-      
 }
